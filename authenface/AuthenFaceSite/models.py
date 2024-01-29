203213f5e58668edf465f528aab459645dbb7727
Edjery Gumbao <edisgumbao@gmail.com>
2024-01-29 09:22:04 +0800

    updated models

--- authenface/AuthenFaceSite/models.py
+++ authenface/AuthenFaceSite/models.py
@@ -5,16 +5,25 @@ class AuthenFaceUser(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.email
+
 class UserImage(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     userId = models.OneToOneField(AuthenFaceUser, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Snapshots(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     userId = models.OneToOneField(AuthenFaceUser, on_delete=models.CASCADE)
     createdAt = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Websites(models.Model):
     id = models.AutoField(primary_key=True)
@@ -23,3 +32,6 @@ class Websites(models.Model):
     accountName = models.CharField(max_length=50)
     userId = models.OneToOneField(AuthenFaceUser, on_delete=models.CASCADE)
     createdAt = models.DateField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.name
