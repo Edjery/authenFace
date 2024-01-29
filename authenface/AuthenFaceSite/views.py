11e7bdc9ba9ccac2ee14d370469557a4c7553b80
Edjery Gumbao <edisgumbao@gmail.com>
2024-01-29 11:48:13 +0800

    refactored views

--- authenface/AuthenFaceSite/views.py
+++ authenface/AuthenFaceSite/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status 
 from .serializers import AuthenFaceUserSerializer
 from .models import AuthenFaceUser, Snapshot, UserImage, Website
 
@@ -15,12 +16,15 @@ def userList(request):
 @api_view(['GET', 'PUT'])
 def user(request, id):
     authenFaceUser = get_object_or_404(AuthenFaceUser, pk=id)
-    if request.method == "GET":
-        serializer = AuthenFaceUserSerializer(authenFaceUser)
-        return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = AuthenFaceUserSerializer(authenFaceUser, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+    match request.method:
+        case "GET":
+            serializer = AuthenFaceUserSerializer(authenFaceUser)
+            return Response(serializer.data)
+        case "PUT":
+            serializer = AuthenFaceUserSerializer(authenFaceUser, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        case _:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
