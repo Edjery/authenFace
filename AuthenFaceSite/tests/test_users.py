a6a3b93156608775cacc2ff05890b0c4ae71834e
Edjery Gumbao <edisgumbao@gmail.com>
2024-01-30 09:38:25 +0800

    created 1 test

--- AuthenFaceSite/tests/test_users.py
+++ AuthenFaceSite/tests/test_users.py
@@ -2,9 +2,9 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 class TestUser:
-    def test_if_user_is_anonymous_returns_401():
+    def test_posting_user_and_returning_404(self):
         client = APIClient()
         newUser = { 'email': 'ed@gmail.com', 'passowrd': '12345' }
         response = client.post('AuthenFaceSite/users', newUser)
 
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.status_code == status.HTTP_404_NOT_FOUND
