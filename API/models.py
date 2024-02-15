694626a4fab7453f9502c9981a6d4e1ad72223c3
Edjery Gumbao <edisgumbao@gmail.com>
2024-02-15 08:46:22 +0800

    changed foreign key and updated pagination

--- API/models.py
+++ API/models.py
@@ -16,7 +16,7 @@ class UserImage(models.Model):
 
 class Snapshot(models.Model):
     name = models.CharField(max_length=50)
-    user = models.OneToOneField(AuthenFaceUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(AuthenFaceUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
@@ -26,7 +26,7 @@ class Website(models.Model):
     name = models.CharField(max_length=50)
     url = models.CharField(max_length=100)
     account_name = models.CharField(max_length=50)
-    user = models.OneToOneField(AuthenFaceUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(AuthenFaceUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
--- API/pagination.py
+++ API/pagination.py
@@ -2,3 +2,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class DefaultPagination(PageNumberPagination):
   page_size = 10
+  page_size_query_param = 'pagesize'
+  max_page_size = 100
