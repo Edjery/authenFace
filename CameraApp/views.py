4e6886e3cd617b4d816385f8c88d005449181ee4
EJ <edisgumbao@gmail.com>
2024-03-06 15:37:52 +0800

    add exp time in token, add login and update register

--- CameraApp/views.py
+++ CameraApp/views.py
@@ -1,9 +1,13 @@
 from django.utils import timezone
 
+from django.core import serializers
 from django.http import StreamingHttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from CameraApp.camera import VideoCamera 
+from datetime import datetime, timedelta
 
+import django.contrib.auth.password_validation as validators
+from django.core import exceptions
 from django.contrib.auth.hashers import check_password, make_password
 from API.models import AuthenFaceUser
 from API.serializers import AuthenFaceUserSerializer
@@ -31,63 +35,79 @@ def video_feed(request):
     return StreamingHttpResponse(gen(request, VideoCamera(user_name)),
         content_type='multipart/x-mixed-replace; boundary=frame')
 
-def generate_jwt_token(user_id, email):
-    payload = {'id': user_id, 'email': email}
+def generate_jwt_token(user_id, email, expiration_time_minutes = 15):
+    expiration_time = datetime.utcnow() + timedelta(minutes=expiration_time_minutes)
+    payload = {'id': user_id, 'email': email, 'exp' : expiration_time}
     jwt_token = jwt.encode(payload, settings.JWT_SECRET, algorithm='HS256')
     return jwt_token
 
 @api_view(['POST'])
-def login_user (request):
-    pass
-#     if request.method == "POST":
-#         email = request.POST.get('email')
-#         password = request.POST.get('password')
+def login_user(request):
+    if request.method == 'POST':
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = get_object_or_404(AuthenFaceUser, email=email)
+        if user and check_password(password, user.password):
+            token = generate_jwt_token(user.id, user.email)
+            userData = {
+                'id': user.id,
+                'email': user.email,
+                'name': user.name
+            }
 
-#         user = get_object_or_404(AuthenFaceUser, email=email)
-#         if user and check_password(password, user.password):
-#             login(request, user)
-#             return JsonResponse({'success': True, 'message': 'Login successful'})
-#         else:
-#             return JsonResponse({'success': False, 'message': 'Invalid username or password'}, status=400)
-#     else:
-#         return JsonResponse({'success': False, 'message': 'Only POST requests are allowed'}, status=405)
+            return Response({'message': 'User registered successfully', 'token' : token, 'userData' : userData}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+    else:
+        return Response({'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 def create_image_name(name): # TODO fix this later
     timestamp = timezone.now().strftime('%Y%m%d%H%M%S')
-    return f'{name}_profile_image_{timestamp}.png'
+    return f'{name.lower()}_profile_image_{timestamp}.png'
 
 @api_view(['POST'])
 def register_user(request):
     if request.method == 'POST':
-        if request.data.get('password') != request.data.get('confirmPassword'):
-            return Response({'error' : 'password did not match'}, status=status.HTTP_400_BAD_REQUEST)          
-
         image_file = request.data.get('userImage')
         userName = request.data.get('name')
+        password = request.data.get('password')
         userImageFileName = create_image_name(userName)
+    
+        if password != request.data.get('confirmPassword'):
+            return Response({'error' : 'password did not match'}, status=status.HTTP_400_BAD_REQUEST)          
 
-        userData = {
+        try:
+            validators.validate_password(password)
+        except exceptions.ValidationError as e:
+            return Response({'error': e.messages}, status=status.HTTP_400_BAD_REQUEST)
+
+        userInput = {
             'name': userName,
             'email': request.data.get('email'),
-            'password': request.data.get('password'),
+            'password': password,
             'userImageName': userImageFileName
         }
 
-        serializer = AuthenFaceUserSerializer(data=userData)
+        serializer = AuthenFaceUserSerializer(data=userInput)
 
         if serializer.is_valid() and image_file:
             user = serializer.save()
             userObject = AuthenFaceUser.objects.get(id = user.id)
             userObject.password = make_password(user.password)
             userObject.save()
-            print('password', userObject.password)
             token = generate_jwt_token(user.id, user.email)
 
             with open('Media/UserImages/' + userImageFileName, 'wb+') as destination:
                 for chunk in image_file.chunks():
                     destination.write(chunk)
+            
+            userData = {
+                'id': user.id,
+                'email': user.email,
+                'name': user.name
+            }
 
-            return Response({'message': 'User registered successfully', 'token' : token}, status=status.HTTP_201_CREATED)
+            return Response({'message': 'User registered successfully', 'token' : token, 'userData' : userData}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     else:
