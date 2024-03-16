b4b3fd7b4a4821fddfabeb72e468fcc5067b7e2c
EJ <edisgumbao@gmail.com>
2024-03-16 18:43:33 +0800

    Update Serializer's error

--- API/serializers.py
+++ API/serializers.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import password_validation
+from django.core import exceptions
 
 from .models import AuthenFaceUser, Snapshot, Website, DummyUser
 
+from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
+from rest_framework import status
 
 class AuthenFaceUserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
@@ -13,7 +17,14 @@ class AuthenFaceUserSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         if 'password' in validated_data:
-            validated_data['password'] = make_password(validated_data['password'])
+            password = validated_data['password']
+
+            try:
+                password_validation.validate_password(password)
+            except exceptions.ValidationError as e:
+                raise ValidationError(detail={'error': e.messages}, code=status.HTTP_400_BAD_REQUEST)
+
+            validated_data['password'] = make_password(password)
         return super().update(instance, validated_data)
      
     class Meta:
